387b856386a97f207ce9a5c025724ca7d5f897ba
George Eaton <george_eaton@hotmail.co.uk>
2016-10-26 11:46:37 +0100

    loading from csv file

--- directory.rb
+++ directory.rb
@@ -5,6 +5,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -63,6 +64,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -109,6 +112,16 @@ def save_students
     csv_line = student_data.join(",")
     #write each csv line to the file "students.csv"
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort}
+    #have left this as a string so it matches cohorts array, rather than converted to symbol
   end
   file.close
 end
